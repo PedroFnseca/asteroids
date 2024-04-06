8dfba32ab49d364e9c8c5adce38418fb0e195a12
pedrofnseca <pedropereira.pp264@gmail.com>
2024-04-06 16:49:28 -0300

    init: main archives

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+const widthScreen = window.innerWidth;
+const heightScreen = window.innerHeight;
+
+function setup() {
+  createCanvas(widthScreen, heightScreen);
+}
+
+function draw() {
+  background(20);
+}
